68559e0a9ec8e86b6dd50e0d43c8b675934e339d
Bowen Li <bowenli1024@gmail.com>
2025-08-27 10:23:09 -0400

    addressed Tom's last comment

--- run_autoformat.sh
+++ run_autoformat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python -m black . --exclude "(third-party|\.venv)"
-docformatter -i -r . --exclude venv .venv third-party
-isort . --skip-gitignore --extend-skip-glob="third-party/*,.venv/*"
+python -m black .
+docformatter -i -r .
+isort .
